0fa98be94847fb70ff5cf71d3d29ff8c4347c8f7
thewsauce <81601736+thewsauce@users.noreply.github.com>
2025-11-10 20:08:17 -0800

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
-// vite.config.js
 import { defineConfig } from "vite";
 
-// CHANGE THIS to your repo name (the bit after your username):
-const repo = "pro-bending-manager"; // <-- e.g. "pro-bending-manager"
-
 export default defineConfig({
-  base: `/${repo}/`, // Pages serves from /<repo>/
+  base: "/pro-bending-manager/", // change this to your actual repo name
 });
